6cd17d9478bb0a9977f7c40a7c21b32532a40f27
Gary Beardsley <tiresandplanes@gmail.com>
2020-04-03 01:39:26 -0400

    Add basic csv reading in covid-gui.py

--- covid-gui.py
+++ covid-gui.py
@@ -1,33 +1,18 @@
 import sys
 import random
 from PySide2 import QtCore, QtWidgets, QtGui
-
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World")
-        self.text.setAlignment(QtCore.Qt.AlignCenter)
-
-        self.layout = QtWidgets.QVBoxLayout()
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-        self.setLayout(self.layout)
-
-        self.button.clicked.connect(self.magic)
+import argparse
+import pandas as pd
 
 
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+
+def read_data(fname):
+    return pd.read_csv(fname)
+
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
-
-    sys.exit(app.exec_())  
+    options = argparse.ArgumentParser()
+    options.add_argument("-f", "--file", type=str, required=True)
+    args = options.parse_args()
+    data = read_data(args.file)
+    print(data)

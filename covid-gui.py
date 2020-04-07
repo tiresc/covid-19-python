b5acba515982bec9a5d71cf30e6b00b6b1e0be63
Gary Beardsley <tiresandplanes@gmail.com>
2020-04-07 10:50:08 -0400

    Add Download of git repo when button pressed

--- covid-gui.py
+++ covid-gui.py
@@ -1,16 +1,37 @@
 import sys
-import random
-from PySide2 import QtCore, QtWidgets, QtGui
-import argparse
-import pandas as pd
-
-def read_data(fname):
-    return pd.read_csv(fname)
+import os
+from git_clone import git_clone
+from PySide2.QtWidgets import (QLineEdit, QPushButton, QApplication,
+    QVBoxLayout, QDialog)
 
 
-if __name__ == "__main__":
-    options = argparse.ArgumentParser()
-    options.add_argument("-f", "--file", type=str, required=True)
-    args = options.parse_args()
-    data = read_data(args.file)
-    print(data)
+class Form(QDialog):
+
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        # Create widgets
+        self.edit = QLineEdit("update?")
+        self.button = QPushButton("Update!")
+        # Create layout and add widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        # Set dialog layout
+        self.setLayout(layout)
+        # Add button signal to greetings slot
+        self.button.clicked.connect(self.greetings)
+
+    # Greets the user
+    def greetings(self):
+            print("Covid-19 data Exists!")
+            os.system('rmdir /Q /s COVID-19')
+            git_clone('https://github.com/CSSEGISandData/COVID-19.git')
+
+if __name__ == '__main__':
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form()
+    form.show()
+    # Run the main Qt loop
+    sys.exit(app.exec_())
